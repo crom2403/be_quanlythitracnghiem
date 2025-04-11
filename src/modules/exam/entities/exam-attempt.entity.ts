2770b179102902f718ac1eae4423d2be9956d17c
tsondev <thanhsonrau2002@gmail.com>
2025-04-11 11:33:57 +0700

    fix

--- src/modules/exam/entities/exam-attempt.entity.ts
+++ src/modules/exam/entities/exam-attempt.entity.ts
@@ -29,6 +29,9 @@ export class ExamAttempt {
   @Column()
   tab_switch_count: number;
 
+  @Column()
+  is_selected: boolean;
+
   @ManyToOne(() => Exam, (exam) => exam.exam_attempts)
   exam: Exam;
 
